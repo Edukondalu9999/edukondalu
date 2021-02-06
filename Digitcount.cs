a75a893613b43c6578cd81dfe694847ba40c6e33
Edukondalu9999 <78524505+Edukondalu9999@users.noreply.github.com>
2021-02-06 12:28:43 +0530

    Add files via upload

---
+++ Digitcount.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet
+{
+    class Digitcount
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 67895;
+            int count = 0;
+            while (num != 0)
+
+            {
+                int digit = num % 10;
+                count++;
+                num = num / 10;
+
+            }
+            Console.WriteLine($"Digit count is {count}");
+        }
+    }
+}
