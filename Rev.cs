197a3eeec148e5542120d48b2e1ecbec3ddaa135
Edukondalu9999 <78524505+Edukondalu9999@users.noreply.github.com>
2021-02-06 10:09:09 +0530

    Add files via upload

---
+++ Rev.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet
+{
+    public class Rev
+    {
+        public static void Main(string[] args)
+        {
+            int n=123, reverse = 0, rem;
+            
+            
+
+            while (n != 0)
+            {
+                rem = n % 10;
+                reverse = reverse * 10 + rem;
+                n /= 10;
+            }
+            Console.Write("Reversed Number: " + reverse);
+        }
+    }
+}
+
+
+
