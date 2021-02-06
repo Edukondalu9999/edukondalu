e2144c1668164254697585b4fff3bc9efcb80d77
Edukondalu9999 <78524505+Edukondalu9999@users.noreply.github.com>
2021-02-06 12:56:47 +0530

    Add files via upload

---
+++ Abhi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet
+{
+    class Abhi
+    {
+        static void Main(string[] args)
+        {
+            int num = 54;
+            Console.WriteLine(num % 10);
+            Console.WriteLine(num / 10);
+            Console.WriteLine(num % 10 + num / 10);
+        }
+    }
+}
