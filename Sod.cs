1db6de29b19a458a7d8481996eb3e4e8ebb6a512
Edukondalu9999 <78524505+Edukondalu9999@users.noreply.github.com>
2021-02-06 12:07:05 +0530

    Add files via upload

---
+++ Sod.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet
+{
+    class Sod { 
+     static void Main(string[] args)
+    {
+        int num = 546;
+        int sum = 0;
+        while (num > 0)
+        {
+            int digit = num % 10;
+            sum = sum + digit;
+            num = num / 10;
+        }
+        Console.WriteLine("Sum of the digits :{0}", sum);
+    }
+}
+}
