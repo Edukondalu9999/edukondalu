a99017f678d493c7d7b22becee4f79b516581684
Edukondalu9999 <78524505+Edukondalu9999@users.noreply.github.com>
2021-02-06 12:25:36 +0530

    Add files via upload

---
+++ String.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet
+{
+    class String
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string1 : ");
+            string str1 = Console.ReadLine();
+            Console.Write("Enter string2 : ");
+            string str2 = Console.ReadLine();
+            Console.WriteLine(str1 + str2);
+        }
+    }
+}
+
