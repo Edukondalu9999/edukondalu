c7b996a0c6a86d421c2a3e21706d2a796dec3ff4
Edukondalu9999 <78524505+Edukondalu9999@users.noreply.github.com>
2021-02-11 12:01:20 +0530

    Add files via upload

---
+++ GetDiffOfDigits.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet
+{
+    class GetDiffOfDigits
+    {
+
+        public static int GetDiffOfDigit(int num)
+        {
+            if (num > 99)
+            {
+                return -2;
+            }
+            if (num < 0)
+            {
+                return -3;
+            }
+            if (num >= 0 && num <= 9)
+            {
+                return -1;
+            }
+            return num / 10 - num % 10;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetDiffOfDigit(38));
+            Console.WriteLine(GetDiffOfDigit(-4));
+            Console.WriteLine(GetDiffOfDigit(100));
+        }
+    }
+}
---
+++ GetSumOfDigit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet
+{
+    class GetSumOfDigit
+    {
+
+        public static int GetSumOfDigits(int num)
+        {
+            if (num > 99)
+            {
+                return -2;
+            }
+            if (num < 0)
+            {
+                return -3;
+            }
+            if (num >= 0 && num <= 9)
+            {
+                return -1;
+            }
+            return num%10+num/10;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetSumOfDigits(34));
+            Console.WriteLine(GetSumOfDigits(-1));
+            Console.WriteLine(GetSumOfDigits(100));
+        }
+    }
+}
