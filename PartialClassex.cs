c8b741f29cd9edf69ba95785fd982757a403da5d
Edukondalu9999 <78524505+Edukondalu9999@users.noreply.github.com>
2021-02-18 11:22:16 +0530

    Add files via upload

---
+++ PartialClassex.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet
+{
+    class PartialClassex
+    {
+        static void Main(string[] args)
+        {
+            Bank p = new Bank();
+            
+            p.CheckBalance();
+            p.MiniStatement();
+        }
+    }
+}
