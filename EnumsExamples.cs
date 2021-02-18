3b9ef33d3bbeaecaabc6ab16b4bb5fd20315ee16
Edukondalu9999 <78524505+Edukondalu9999@users.noreply.github.com>
2021-02-18 11:37:02 +0530

    Add files via upload

---
+++ EnumsExamples.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dotnet
+{
+    class EnumsExamples
+    {
+        enum days { sunday, monday = 111, tuesday, wedesday, thursday, friday, saturday };
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine($"{days.sunday}=>{(int)days.sunday}");
+            Console.WriteLine($"{days.monday}=>{(int)days.monday}");
+            Console.WriteLine($"{days.tuesday}=>{(int)days.tuesday}");
+            Console.WriteLine($"{days.wedesday}=>{(int)days.wedesday}");
+
+
+        }
+    }
+}
